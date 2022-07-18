2515ff407908cbbaf0d4f3f53ce0d75a83dfbb7f
Shrikant Dhanawade <sdhanawade@corp.qualys.com>
2022-07-18 14:26:56 +0530

    Added new AWS SG Template

---
+++ aws/security-group/aws_security_group_rule.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group_rule" "rule_port_22" {
+  type = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "sg-03732eaaaa344530d"
+}
+
+resource "aws_security_group_rule" "rule_port_3389" {
+  type = "ingress"
+  from_port         = 3389
+  to_port           = 3389
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "sg-03732eaaaa344530d"
+}
