0669652094e252022df8d42de614488d9fcf1a62
shrikantdhanawade-github <65462025+shrikantdhanawade-github@users.noreply.github.com>
2022-08-09 13:20:18 +0530

    Update aws_security_group_rule.tf

--- aws/security-group/aws_security_group_rule.tf
+++ aws/security-group/aws_security_group_rule.tf
@@ -3,7 +3,7 @@ resource "aws_security_group_rule" "rule_port_22" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["103.97.242.16/32"]
+  cidr_blocks       = ["103.97.242.17/32"]
   security_group_id = "sg-03732eaaaa344530d"
 }
 
@@ -12,7 +12,7 @@ resource "aws_security_group_rule" "rule_port_3389" {
   from_port         = 3389
   to_port           = 3389
   protocol          = "tcp"
-  cidr_blocks       = ["103.97.242.16/32"]
+  cidr_blocks       = ["103.97.242.17/32"]
   security_group_id = "sg-03732eaaaa344530d"
 }
 
